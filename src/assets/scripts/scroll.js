70a17df5fe4499d539bc4857e79094d464882d3a
Roberto Neves <robertonsilva@gmail.com>
2024-01-24 11:33:39 -0300

    Finalizado HTML, CSS, Lista e Profile

--- src/assets/scripts/scroll.js
+++ src/assets/scripts/scroll.js
@@ -8,6 +8,7 @@ var scrollDirection=0;
 
 function toggleComponents() {
   const headerBar = document.getElementById('header');
+  const menuProfile = document.getElementById('menuprofile');
   
   let currentScroll = window.scrollY ||  document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
   var scrollUp = scrollDirection >= currentScroll || currentScroll <= 160;    
@@ -18,6 +19,33 @@ function toggleComponents() {
     headerBar.className =  headerBar.className.replace(' hideheader', '')    
   }
 
+  if(menuProfile){           
+    const profileBar = document.querySelector('.profile-image');    
+    const profileBarHeight = profileBar.getBoundingClientRect().height
+    const headerBarHeight = headerBar.getBoundingClientRect().height
+    const menuBarHeight = menuProfile.getBoundingClientRect().height
+    
+    if (currentScroll >= profileBarHeight + headerBarHeight + menuBarHeight && !menuProfile.className.includes("pinned")) {
+      menuProfile.className += ' pinned';
+    } else if (currentScroll < profileBarHeight + headerBarHeight + menuBarHeight) {
+      menuProfile.className = menuProfile.className.replace(' hidebar', '');
+      menuProfile.className = menuProfile.className.replace(' pinned', '');
+      menuProfile.className = menuProfile.className.replace(' scrolledup', '');   
+    }
+
+    if (!scrollUp && currentScroll >= (profileBarHeight + headerBarHeight) && !menuProfile.className.includes("hidebar") ){
+      menuProfile.className += ' hidebar';
+      menuProfile.className = menuProfile.className.replace(' scrolledup', '');
+    } else if (scrollUp) {
+      menuProfile.className =  menuProfile.className.replace(' hidebar', '');   
+      if (menuProfile.className.includes("pinned") && !menuProfile.className.includes("scrolledup"))      
+      {
+        menuProfile.className += ' scrolledup';
+      }       
+    }
+  }
+ 
+
   scrollDirection = currentScroll;
 
   var btnTop = document.getElementById("top-btn-id");
@@ -26,6 +54,15 @@ function toggleComponents() {
   } else {
     btnTop.style.display = "none";
   }
+
+  var barContact = document.querySelector(".contact-bar");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    barContact.style.display = "";
+  } else {
+    barContact.style.display = "none";
+  }
+
+ 
 
   //var btnPhone = document.getElementById("phone-id");
   //var btnWts = document.getElementById("wts-id");
